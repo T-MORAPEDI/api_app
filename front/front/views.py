96feb4d7570be66a59944dd6a4a8b1caf127b7cb
T.D MORAPEDI <101623837+T-MORAPEDI@users.noreply.github.com>
2023-10-08 17:17:06 +0200

    Update views.py

--- front/front/views.py
+++ front/front/views.py
@@ -12,18 +12,12 @@ def analyze_sentiment(request):
         predicted_class = predict_class(text)
 
         # Mapping the predicted class to sentiment class
-        if predicted_class == 5:
-            sentiment_class = 'Strong Positive'
-        elif predicted_class == 4:
-            sentiment_class = 'Positive'
-        elif predicted_class == 3:
-            sentiment_class = 'Neutral'
-        elif predicted_class == 2:
+        if predicted_class > 0.5:
             sentiment_class = 'Negative'
-        elif predicted_class == 1:
-            sentiment_class = 'Strong Negative'
+        elif predicted_class == 0.5:
+            sentiment_class = 'Neutral'
         else:
-            sentiment_class = 'Unknown'  # Handle unexpected class values
+            sentiment_class = 'Positive'
 
         context = {
             'class': sentiment_class,
